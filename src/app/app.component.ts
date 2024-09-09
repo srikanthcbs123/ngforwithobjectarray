dc88175dad83ac3d77255fbbed3d64ca70456823
srikanthcbs123 <srikanthcbs123@gmail.com>
2024-09-09 08:51:48 +0530

    userstory123456 employee class created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Employee } from './employee';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngforwithobjectarray';
+  employees:Employee[]=
+  [
+   new Employee(1,"user1",101),
+   new Employee(2,"user2",102),
+   new Employee(3,"user3",103),
+   new Employee(4,"user4",104),
+   new Employee(5,"user5",105),
+   new Employee(6,"user6",106),
+   new Employee(7,"user7",107),
+   new Employee(8,"user8",108),
+   new Employee(9,"user9",109),
+   new Employee(10,"user10",110),
+   new Employee(11,"user11",111),
+   new Employee(12,"user12",112)
+  ];
+  constructor()
+  {
+    debugger;
+    console.log("employees data array started");
+    console.log(this.employees);
+    console.log("employees data array ended");
+    console.log(10+50);
+  }
+ 
+ 
 }
---
+++ src/app/employee.ts
@@ -0,0 +1,10 @@
+export class Employee {
+    empid: number;
+    empname: string;
+    salary: number;
+    constructor(id: number, name: string, salary: number) {
+        this.empid = id;
+        this.empname = name;
+        this.salary = salary;
+    }
+}
